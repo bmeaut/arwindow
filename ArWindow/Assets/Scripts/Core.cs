a36c9abcbbacd7e1936be18dc12ed86a9a93e4ba
Bence Somos <somos.bence@autsoft.hu>
2020-07-03 12:38:28 +0200

    debug visualization of camera perspective and window size

--- ArWindow/Assets/Scripts/Core.cs
+++ ArWindow/Assets/Scripts/Core.cs
@@ -28,7 +28,15 @@ public class Core : MonoBehaviour
 
         var vleft = (left - playerData.EyePosition).normalized;
         var vright = (right - playerData.EyePosition).normalized;
-        renderCamera.fieldOfView = Mathf.Acos(Vector3.Dot(vleft, vright)) / Mathf.PI * 180f;
 
+        Debug.DrawRay(playerData.EyePosition, vleft * 1000, Color.green);
+        Debug.DrawRay(playerData.EyePosition, vright * 1000, Color.blue);
+        
+        renderCamera.fieldOfView = Vector3.Angle(vleft, vright); //Mathf.Acos(Vector3.Dot(vleft, vright)) / Mathf.PI * 180f;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(windowCenter.position, new Vector3(WindowConfiguration.Instance.Width, WindowConfiguration.Instance.Height, 0.01f));
     }
 }
