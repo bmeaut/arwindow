e2fb92819602edd41262c80ccb428576fd33f48f
Bálint Nagy <info.balint.nagy@edu.bme.hu>
2022-12-05 11:41:45 +0100

    Add landmark tracking

---
+++ ArWindow/Assets/Scripts/PlayerManagement/IFaceLandmarkProvider.cs
@@ -0,0 +1,14 @@
+﻿using ARWindow.PlayerManagement;
+using Emgu.CV.Util;
+using System.Drawing;
+using UnityEngine;
+
+namespace Assets.Scripts.PlayerManagement
+{
+    public interface IFaceLandmarkProvider
+    {
+        VectorOfVectorOfPointF GetLandmarks();
+        Vector3 GetFacePosition();
+        Rectangle GetFaceRect();
+    }
+}
